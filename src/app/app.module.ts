c8b4899bc980bbd2b5971a38d589984810be56ee
Muhammad Ali <muhammadali@192.168.1.5>
2023-09-14 04:16:29 +0500

    :tada: UPDATE: initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgWeatherWidgetModule } from 'ng-weather-widget';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, NgWeatherWidgetModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
