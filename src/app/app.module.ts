5bcb9e8d93ed8c752e810ed425888b15626efa3a
Muhammad Ali <muhammadwarraich007@gmail.com>
2023-09-14 19:21:34 +0500

    :recycle: UPDATE: upgraded version of lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgWeatherWidgetModule } from 'ng-weather-widget';
 import { AppComponent } from './app.component';
-
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, NgWeatherWidgetModule],
